f53c6b9669843d98dca744fea9c1cfa25a723c69
Akash <158745842+Akash8t2@users.noreply.github.com>
2024-03-19 14:27:34 +0530

    Update start.py

--- X/modules/bot/start.py
+++ X/modules/bot/start.py
@@ -15,7 +15,7 @@ def pmowner(client, callback_query):
     client.answer_callback_query(callback_query.id, text="Message sent")
 
 logoX = [
-    "https://graph.org/file/83978974fe5be2da118d7.jpg"
+    "https://graph.org/file/a8a749ce5c45cef15e347.jpg"
 ]
 
 alive_logo = random.choice(logoX)
@@ -24,12 +24,12 @@ alive_logo = random.choice(logoX)
 async def start(app, message):
     chat_id = message.chat.id
     file_id = alive_logo
-    caption = "𝐇𝐢, 𝐈 𝐚𝐦 𝐀𝐬𝐢𝐬𝐬𝐭𝐚𝐧𝐭 𝐉𝐚𝐩𝐚𝐧𝐞𝐬𝐞-𝐗-𝐔𝐬𝐞𝐫𝐛𝐨𝐭\n 𝐖𝐡𝐚𝐭 𝐛𝐫𝐨? 𝐈𝐟 𝐲𝐨𝐮 𝐰𝐚𝐧𝐭 𝐭𝐨 𝐛𝐞 𝐚𝐧𝐠𝐫𝐲, 𝐲𝐨𝐮 𝐫𝐞𝐚𝐥𝐥𝐲 𝐡𝐚𝐯𝐞 𝐚 𝐭𝐚𝐭𝐭𝐨𝐨?."
+    caption = "𝐇𝐢, 𝐈 𝐚𝐦 𝐀𝐬𝐢𝐬𝐬𝐭𝐚𝐧𝐭 𝐀𝐊-𝐊𝐈𝐍𝐆-𝐔𝐬𝐞𝐫𝐛𝐨𝐭\n 𝐖𝐡𝐚𝐭 𝐛𝐫𝐨? 𝐈𝐟 𝐲𝐨𝐮 𝐰𝐚𝐧𝐭 𝐭𝐨 𝐛𝐞 𝐚𝐧𝐠𝐫𝐲, 𝐲𝐨𝐮 𝐫𝐞𝐚𝐥𝐥𝐲 𝐡𝐚𝐯𝐞 𝐚 𝐭𝐚𝐭𝐭𝐨𝐨?."
     reply_markup = InlineKeyboardMarkup([
         [
-            InlineKeyboardButton("𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url="https://t.me/Japanese_Userbot_Chat"),
-            InlineKeyboardButton("𝐂𝐡𝐚𝐧𝐧𝐞𝐥", url="https://t.me/Japanese_Useebot"),
-            InlineKeyboardButton("𝐎𝐰𝐧𝐞𝐫", url="https://t.me/Nobitaa_xd"),
+            InlineKeyboardButton("𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url="https://t.me/Ak_support1"),
+            InlineKeyboardButton("𝐂𝐡𝐚𝐧𝐧𝐞𝐥", url="https://t.me/Ak_support1"),
+            InlineKeyboardButton("ᴍᴀʟɪᴋ", url="https://t.me/Alllllllllllllllllllllll"),
         ],
     ])
 
